6ca27f9fa1171f048f6a5f3f55bba3d3ee377c0e
Ojima Hikaru <ojiam.h@gmail.com>
2012-10-30 20:18:57 +0900

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mychelin::Application.config.secret_token = 'dc21300c93f15ea2f0f8069d3be7e67cf31f788a68ddabfe59c716fbde86f33bf1f3f1b0d12b159864c040d4d5146f57f1a84fd6af55672ec9d8620e928ffb9c'
