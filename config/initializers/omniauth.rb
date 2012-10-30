fd5da8cb70a98391ca76fb486ccef253c805fdd7
Ojima Hikaru <ojiam.h@gmail.com>
2012-10-31 02:37:59 +0900

    Remove keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,3 @@
-ENV['OMNIAUTH_PROVIDER_KEY'] = '431080720288920'
-ENV['OMNIAUTH_PROVIDER_SECRET'] = 'f5f70aec3ed9d8378f0419698843f4d6'
-
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET']
 end
