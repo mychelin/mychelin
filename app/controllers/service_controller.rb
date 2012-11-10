05e1a4dbfc99a5b4eb36154f0fd0070e8bed367d
Ojima Hikaru <ojiam.h@gmail.com>
2012-11-10 20:05:15 +0900

    Refactored service_controller.

--- app/controllers/service_controller.rb
+++ app/controllers/service_controller.rb
@@ -6,24 +6,13 @@ class ServiceController < ApplicationController
     longitude = params[:longitude]
 
     if user_signed_in? && (latitude && longitude)
-      host = "maps.googleapis.com"
-      port = 443
       path = "/maps/api/place/search/json"
-      api_key = Mychelin::Application.config.google_api_key
-      query = "location=#{latitude},#{longitude}&radius=500&types=food&language=ja&sensor=true&key=#{api_key}"
-
-      https = if ENV['https_proxy'].present?
-                u = URI.parse(ENV['http_proxy'])
-                Net::HTTP::Proxy(u.host, u.port)
-              else
-                Net::HTTP
-              end
-      https = https.new(host, port)
-      https.use_ssl = true
-
-      response = https.start do |h|
-        h.get(path + '?' + query)
-      end
+      query = { location: "#{latitude},#{longitude}",
+                radius:   500,
+                types:     'food',
+                language: 'ja',
+                sensor:   true }
+      response = proxy_google path, query
     end
 
     if response
@@ -41,33 +30,42 @@ class ServiceController < ApplicationController
     reference = params[:reference]
 
     if user_signed_in? && reference
-      host = "maps.googleapis.com"
-      port = 443
       path = "/maps/api/place/details/json"
-      api_key = Mychelin::Application.config.google_api_key
-      query = "reference=#{reference}&language=ja&sensor=true&key=#{api_key}"
+      query = { reference: reference,
+                language:  'ja',
+                sensor:    true }
+      response = proxy_google path, query
+    end
 
-      https = if ENV['https_proxy'].present?
-                u = URI.parse(ENV['http_proxy'])
-                Net::HTTP::Proxy(u.host, u.port)
-              else
-                Net::HTTP
-              end
-      https = https.new(host, port)
-      https.use_ssl = true
-
-      response = https.start do |h|
-        h.get(path + '?' + query)
-      end
-      if response
-        if response.code.to_i < 400
-          render json: response.body
-        else
-          render json: '', status: response.code.to_i
-        end
+    if response
+      if response.code.to_i < 400
+        render json: response.body
       else
-        render json: '', status: 401 # unauthorized
+        render json: '', status: response.code.to_i
       end
+    else
+      render json: '', status: 401 # unauthorized
+    end
+  end
+
+  private
+  def proxy_google(path, query_hash)
+    host = "maps.googleapis.com"
+    port = 443
+
+    query_hash[:key] = Mychelin::Application.config.google_api_key
+
+    https = if ENV['https_proxy'].present?
+              u = URI.parse(ENV['http_proxy'])
+              Net::HTTP::Proxy(u.host, u.port)
+            else
+              Net::HTTP
+            end
+    https = https.new(host, port)
+    https.use_ssl = true
+
+    https.start do |h|
+      h.get(path + '?' + query_hash.to_query)
     end
   end
 end
