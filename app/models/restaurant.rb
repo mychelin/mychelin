03d65481819c3ccbcc3d7fc021b1af2b1d1b6eda
kotapon <kotapon@h111.165.226.10.105163.vlan.kuins.net>
2012-11-21 13:16:00 +0900

    tel restriction

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,4 @@
 class Restaurant < ActiveRecord::Base
   attr_accessible :address, :business_hour, :capacity, :credit, :holiday, :location, :name, :parking, :tel, :url
+  validates :tel, :format => { :with => /(\d|-)+/ }
 end
